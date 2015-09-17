acb68b0abb57a7972e8edcd504db2be0332e06b1
Jay-Jay-D <jjdambrosio@gmail.com>
2015-09-16 23:18:31 -0300

    My silly algo added

---
+++ Algorithm.CSharp/JJAlgorithms/IntradayBuyLow/BuyLowStrategy.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuantConnect.Algorithm.CSharp.IntradayBuyLow
+{
+    /// <summary>
+    /// This class implement a strategy basd in the following rules:
+    ///     - Read the last M days of trading data. For each day, find the downwards 
+    ///       and upwards "runs" in the smoothed data.
+    ///     - A downward run is a drop with an upward turn around at the end and vice-versa.
+    ///     - Find the largest N runs for each day. 
+    ///     - Average the drop size over the M days to find the drop size to be used by the model.
+    /// </summary>
+    public partial class BuyLowStrategy
+    {
+        # region Fields
+        private int runsPerDay;
+        private int previousDaysN;
+
+        private bool firstDay;
+        private decimal previousValue;
+        private DateTime previousValueDate;
+        
+        private decimal accumulatedRun;
+        private decimal valuePrevToRun;
+        
+        List<decimal> intraDayRuns = new List<decimal>();
+
+        private Queue<decimal> dailyDownwardRuns = new Queue<decimal>();
+        private Queue<decimal> dailyUpwardRuns = new Queue<decimal>();
+        
+        private decimal downwardRunsThreshold;
+        private decimal upwardRunsThreshold;
+
+        private bool isReady;
+        private bool isDownReady;
+        private bool isUpReady;
+        
+        private bool turnAround;
+        private int orderSignal;
+        # endregion
+
+        public bool IsReady
+        {
+          get { return isReady; }
+          set { isReady = value; }
+        }
+
+        public bool TurnAround
+        {
+          get { return turnAround; }
+          set { turnAround = value; }
+        }
+
+        public int OrderSignal
+        {
+          get { return orderSignal; }
+          set { orderSignal = value; }
+        }
+
+        // Constructor
+        public BuyLowStrategy(int PreviousDaysN, int RunsPerDay)
+        {
+            this.previousDaysN = PreviousDaysN;
+            this.runsPerDay = RunsPerDay;
+            
+            // Initializing the fields
+            isReady = false;
+            turnAround = false;
+            orderSignal = 0;
+            firstDay = true;
+        }
+
+        public BuyLowStrategy(int PreviousDaysN, int RunsPerDay, string symbol)
+        {
+            this.previousDaysN = PreviousDaysN;
+            this.runsPerDay = RunsPerDay;
+
+            // Initializing the fields
+            isDownReady = true;
+            isUpReady = true;
+            isReady = true;
+            turnAround = false;
+            orderSignal = 0;
+            firstDay = true;
+
+            // Initializing the previous days runs and the thresholds
+
+            dailyDownwardRuns = WarmUp(symbol, FieldName.downwardRuns);
+            dailyUpwardRuns = WarmUp(symbol, FieldName.upwardRuns);
+
+            downwardRunsThreshold = dailyDownwardRuns.Average();
+            upwardRunsThreshold = dailyUpwardRuns.Average();
+        }
+
+        // Add new serie value
+        public void AddSerieValue(DateTime timeStamp, decimal serieValue)
+        {
+            bool sameDay;
+            DateTime actualValueDate = timeStamp.Date;
+
+            if (firstDay)
+            {
+                // Day initialization
+                InitializeDay(timeStamp, serieValue);
+                firstDay = false;
+            }
+
+            sameDay = actualValueDate == previousValueDate.Date;
+
+            if (sameDay)
+            {
+                // If is the same day, I'll add the value to the actual day and keep going
+                SameDay(timeStamp, serieValue);
+            }
+            else
+            {
+                // If is a new day, etimate the means runs for the day.
+                NewDay();
+                InitializeDay(timeStamp, serieValue);
+
+            }
+        }
+
+        private void InitializeDay(DateTime timeStamp, decimal serieValue)
+        {
+            previousValueDate = timeStamp;
+            valuePrevToRun = serieValue;
+            previousValue = serieValue;
+
+            accumulatedRun = 0;
+
+            turnAround = false;
+            firstDay = false;
+        }
+
+
+        private void SameDay(DateTime timeStamp, decimal serieValue)
+        {
+            decimal valueDiff;
+            decimal brokenRun;
+
+            decimal previousAccum = accumulatedRun;
+
+            valueDiff = serieValue - previousValue;
+
+            /* If the accumulated differences and the new difference has the same signal,
+             then keep accumulating*/
+            if (valueDiff * previousAccum >= 0)
+            {
+                accumulatedRun = previousAccum + valueDiff;
+                turnAround = false;
+            }
+            // If not:
+            else
+            {
+                // the accumulation is the las difference
+                accumulatedRun = valueDiff;
+                // there's a turn around
+                turnAround = true;
+                // estimate the run's lenght and set the denominator for the next run.
+                brokenRun = previousAccum / valuePrevToRun;
+                valuePrevToRun = previousValue;
+
+                // add the new run the today's runs
+                intraDayRuns.Add(brokenRun);
+
+                // check if the run triggers a order.
+                CheckOrders(brokenRun);
+            }
+
+            //update the serie and date.
+            previousValue = serieValue;
+            previousValueDate = timeStamp;
+        }
+
+
+        private void NewDay()
+        {
+            decimal dayMeanDownwardRun;
+            decimal dayMeanUpwardRun;
+
+            // if there's some run data yesterday, then estimate the top runs average 
+            if (intraDayRuns.Count != 0)
+            {
+                // Run two queries to obtain the requier daily run mean
+                dayMeanDownwardRun = (from run in intraDayRuns
+                                      where run < 0
+                                      orderby run ascending
+                                      select run).Take(runsPerDay).Average();
+
+                dayMeanUpwardRun = (from run in intraDayRuns
+                                    where run > 0
+                                    orderby run descending
+                                    select run).Take(runsPerDay).Average();
+
+                dailyDownwardRuns.Enqueue(dayMeanDownwardRun);
+                dailyUpwardRuns.Enqueue(dayMeanUpwardRun);
+                intraDayRuns.Clear();
+                // If there are the required days, then the strategy is ready
+                if (dailyUpwardRuns.Count == previousDaysN) isDownReady = true;
+                if (dailyDownwardRuns.Count == previousDaysN) isUpReady = true;
+                if (isDownReady || isUpReady) isReady = true;
+                // Keep only the required days
+                if (dailyDownwardRuns.Count > previousDaysN)
+                {
+                    dailyDownwardRuns.Dequeue();
+                    dailyUpwardRuns.Dequeue();
+                }
+
+                downwardRunsThreshold = dailyDownwardRuns.Average();
+                upwardRunsThreshold = dailyUpwardRuns.Average();
+            }
+        }
+
+
+        private void CheckOrders(decimal brokenRun)
+        {
+            if (this.isReady)
+            {
+                orderSignal = 0; // Do nothing
+
+                if (brokenRun < downwardRunsThreshold && isDownReady)
+                {
+                    // Long order
+                    orderSignal = 1;
+                }
+                else if (brokenRun > upwardRunsThreshold && isUpReady)
+                {
+                    // Short order
+                    orderSignal = -1;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\nPrevious days downward runs,\n");
+            foreach (var run in dailyDownwardRuns)
+            {
+                sb.Append(run + "m,");
+            }
+            sb.Append("\nPrevious days upward runs,\n");
+            foreach (var run in dailyDownwardRuns)
+            {
+                sb.Append(run + "m,");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Algorithm.CSharp/JJAlgorithms/IntradayBuyLow/IntradayBuyLow1_3.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using QuantConnect.Indicators;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
+using QuantConnect.Securities;
+using QuantConnect.Securities.Equity;
+
+namespace QuantConnect.Algorithm.CSharp.IntradayBuyLow
+{
+    /// <summary>
+    /// Basic template algorithm simply initializes the date range and cash
+    /// </summary>
+    public partial class IntradayBuyLow : QCAlgorithm
+    {
+        private int previousDays = 5;
+        private int runsPerDay = 20;
+        
+        private int HMA_Period = 4;
+
+        private decimal maxLeverage = 3m;
+        private decimal leverageBuffer = 0.2m;
+        
+        private decimal leverage;
+        private decimal maxLeverageValue = 0m;
+
+        bool isMarketOpen = true;
+
+        public static string[] symbols = { "IBM", "SPY" }; /*{ "AAPL", "AMZN", "FB", "GE", "GOOGL", "JNJ", "JPM", "MSFT",
+                                             "NVS", "PFE", "PG", "PTR", "TM", "VZ", "WFC" };*/
+
+        public Dictionary<string, BuyLowStrategy> Strategy = new Dictionary<string, BuyLowStrategy>();
+        
+        #region Share size
+        /*public Dictionary<string, decimal> stockShareSize = new Dictionary<string, decimal>()
+        {
+            {"AAPL",  0.0268808004465086964875867853m},
+            {"AMZN",  0.0284105472150871579422202126m},
+            {"FB",    0.0306514529936117601276322134m},
+            {"GE",    0.0268808004465086964875867853m},
+            {"GOOGL", 0.8361228141074589339991876974m},
+            {"JNJ",   0.0268808004465086964875867853m},
+            {"JPM",   0.0268808004465086964875867853m},
+            {"MSFT",  0.0353489342329171353539834940m},
+            {"NVS",   0.0268808004465086964875867853m},
+            {"PFE",   0.0275923179117485217187767271m},
+            {"PG",    0.0268808004465086964875867853m},
+            {"PTR",   1.1602970385264010880158079282m},
+            {"TM",    0.2000760726268714143378924804m},
+            {"VZ",    0.1464682555368547445190284709m},
+            {"WFC",   0.0737477641699970650599500645m}
+        };*/
+        #endregion
+        
+        EquityExchange Market = new EquityExchange();
+        Dictionary<string, HullMovingAverage> hma = new Dictionary<string, HullMovingAverage>();
+        //Dictionary<string, ExponentialMovingAverage[]> ema = new Dictionary<string, ExponentialMovingAverage[]>();
+
+
+
+        /// <summary>
+        /// Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.
+        /// </summary>
+        public override void Initialize()
+        {
+            SetStartDate(2013, 10, 7);   //Set Start Date
+            SetEndDate(2013, 10, 11);    //Set End Date
+            SetCash(100000);             //Set Strategy Cash
+            
+            foreach (string symbol in symbols)
+            {
+                Strategy.Add(symbol, new BuyLowStrategy(previousDays, runsPerDay));
+                previousStockProfit.Add(symbol, 0m);
+                actualStockProfit.Add(symbol, 0m);
+
+                AddSecurity(SecurityType.Equity, symbol, Resolution.Minute, true, maxLeverage, false);
+                
+                
+                //tradier does 1 dollar equity trades
+                Securities[symbol].TransactionModel = new ConstantFeeTransactionModel(1.00m);
+
+
+                HullMovingAverage HMA = new HullMovingAverage("myHull", 4);
+                Func<BaseData, decimal> selector = null;
+                RegisterIndicator(symbol, HMA, Resolution.Minute, selector);
+
+                hma.Add(symbol, HMA);
+
+                
+                stockShareSize.Add(symbol, maxLeverage / symbols.Length);
+                
+            }
+        }
+
+        /// <summary>
+        /// OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+        /// </summary>
+        /// <param name="data">TradeBars IDictionary object with your stock data</param>
+        public void OnData(TradeBars data)
+        {
+            foreach (string symbol in symbols)
+            {
+                if (!hma[symbol].IsReady) return;
+
+                Strategy[symbol].AddSerieValue(Time, hma[symbol]);
+
+                isMarketOpen = Market.DateTimeIsOpen(Time) && Market.DateTimeIsOpen(Time.AddMinutes(10));
+
+                if (isMarketOpen)
+                {
+                    if (Strategy[symbol].IsReady)
+                    {
+                        // If I have stocks and there's a turn around, then liquidate
+                        if (Portfolio[symbol].HoldStock && Strategy[symbol].TurnAround)
+                        {
+                            Liquidate(symbol);
+                        }
+                        // If I don't have stocks and there's a signal, then operate.
+                        else if (!Portfolio[symbol].HoldStock && Strategy[symbol].OrderSignal != 0)
+                        {
+                            EntryAndSetStopLoss(symbol, Strategy[symbol].OrderSignal);
+                        }
+                    }
+                }
+                // If have stocks and the market is about ot close (15 minutes earlier), liquidate.
+                else if (Portfolio[symbol].HoldStock) Liquidate(symbol);
+            }
+            leverage = Portfolio.TotalHoldingsValue / Portfolio.TotalPortfolioValue;
+            if (leverage > maxLeverageValue)
+            {
+                if (leverage > maxLeverage)
+                {
+                    Log("Leverage exceeds max allowed leverage");
+                    leverageBuffer *= 0.975m;
+                }
+                maxLeverageValue = leverage;
+            }
+        }
+
+
+        private void EntryAndSetStopLoss(string symbol, int signal)
+        {
+            SetHoldings(symbol, stockShareSize[symbol] * -1); // Constanza
+        }
+
+        
+        public override void OnEndOfDay(string symbol)
+        {
+            Log(symbol + Portfolio[symbol].Profit);
+            switch (frequency)
+            {
+                case RebalanceFrequency.Daily:
+                    RebalanceOrderSizes(symbol);
+                    break;
+                case RebalanceFrequency.Weekly:
+                    if (Time.DayOfWeek == DayOfWeek.Tuesday) RebalanceOrderSizes(symbol);
+                    break;
+                case RebalanceFrequency.Monthly:
+                    if (Time.Day == 1) RebalanceOrderSizes(symbol);
+                    break;
+            }
+        }
+
+        public override void OnEndOfAlgorithm()
+        {
+            base.OnEndOfAlgorithm();
+            foreach (string symbol in symbols)
+            {
+                Log("//" + symbol);
+                Log(symbol + Strategy[symbol].ToString());
+                Log(symbol + " share size " + stockShareSize[symbol]);
+            }
+
+        }
+    }
+}
