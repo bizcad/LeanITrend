12f742f2571fae8916f8b3d30094b39244f19b48
Jay-Jay-D <jjdambrosio@gmail.com>
2015-09-26 19:27:41 -0300

    DecycleInverseFisherStrategy Tested

---
+++ Algorithm.CSharp/Common/IStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuantConnect.Indicators;
+using QuantConnect.Data.Market;
+
+namespace QuantConnect.Algorithm.CSharp.Common
+{
+    interface IStrategy
+    {
+        /// <summary>
+        /// Checks the signal.
+        /// </summary>
+        /// <returns></returns>
+        OrderSignal CheckSignal();
+    }
+
+    public abstract class BaseStrategy : IStrategy
+    {
+        public StockState Position;
+
+        public Nullable<decimal> EntryPrice;
+
+        public abstract OrderSignal CheckSignal();
+    }
+}
