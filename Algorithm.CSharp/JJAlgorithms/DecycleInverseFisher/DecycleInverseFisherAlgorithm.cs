ef62cd85491141b4344c3b8782dc412239784797
Jay-Jay-D <jjdambrosio@gmail.com>
2015-09-28 07:19:18 -0300

    DIF logging added

--- Algorithm.CSharp/JJAlgorithms/DecycleInverseFisher/DecycleInverseFisherAlgorithm.cs
+++ Algorithm.CSharp/JJAlgorithms/DecycleInverseFisher/DecycleInverseFisherAlgorithm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +10,15 @@ using QuantConnect.Data.Market;
 using QuantConnect.Data.Consolidators;
 using QuantConnect.Indicators;
 using QuantConnect.Orders;
+using QuantConnect.Securities;
 using QuantConnect.Securities.Equity;
+
 
 namespace QuantConnect.Algorithm.CSharp.JJAlgorithms.DecycleInverseFisher
 {
     public class DecycleInverseFisherAlgorithm : QCAlgorithm
     {
-        #region "Algorithm Globals"
+        #region Algorithm Globals
         private DateTime _startDate = new DateTime(2013, 10, 7);
         private DateTime _endDate = new DateTime(2013, 10, 11);
         private decimal _portfolioAmount = 25000;
@@ -41,13 +44,21 @@ namespace QuantConnect.Algorithm.CSharp.JJAlgorithms.DecycleInverseFisher
         private static string[] Symbols = { "AIG", "BAC", "IBM", "SPY" };
 
         // Dictionary used to store the ITrendStrategy object for each symbol.
-        private Dictionary<string, DIFStrategy> Strategies = new Dictionary<string, DIFStrategy>();
+        private Dictionary<string, DIFStrategy> Strategy = new Dictionary<string, DIFStrategy>();
 
         // Dictionary used to store the portfolio sharesize for each symbol.
         private Dictionary<string, decimal> ShareSize = new Dictionary<string, decimal>();
 
         private EquityExchange theMarket = new EquityExchange();
         #endregion
+
+        #region Logging stuff - Defining
+
+        public List<StringBuilder> stockLogging = new List<StringBuilder>();
+        public StringBuilder portfolioLogging = new StringBuilder();
+        private int barCounter = 0;
+
+        #endregion Logging stuff - Defining
 
         #region QCAlgorithm overriden methods
         public override void Initialize()
@@ -56,12 +67,28 @@ namespace QuantConnect.Algorithm.CSharp.JJAlgorithms.DecycleInverseFisher
             SetEndDate(_endDate);                   //Set End Date
             SetCash(_portfolioAmount);              //Set Strategy Cash
 
+            #region Logging stuff - Initializing Portfolio Logging
+
+            portfolioLogging.AppendLine("Counter, Time, Portfolio Value");
+            int i = 0;  // Only used for logging.
+
+            #endregion Logging stuff - Initializing Portfolio Logging
+
             foreach (string symbol in Symbols)
             {
                 AddSecurity(SecurityType.Equity, symbol, Resolution.Minute);
-                Strategies.Add(symbol, new DIFStrategy(DecyclePeriod, InvFisherPeriod, Threshold, Tolerance));
+                Strategy.Add(symbol, new DIFStrategy(DecyclePeriod, InvFisherPeriod, Threshold, Tolerance));
                 RegisterStrategy(symbol);
                 ShareSize.Add(symbol, (maxLeverage * (1 - leverageBuffer)) / Symbols.Count());
+
+                #region Logging stuff - Initializing Stock Logging
+
+                stockLogging.Add(new StringBuilder());
+                stockLogging[i].AppendLine("Counter, Time, Close, Decycle, InvFisher, OrderSignal, StateFromStrategy, StateFromPorfolio");
+                i++;
+
+                #endregion Logging stuff - Initializing Stock Logging
+
             }
         }
 
@@ -70,6 +97,7 @@ namespace QuantConnect.Algorithm.CSharp.JJAlgorithms.DecycleInverseFisher
             bool isMarketAboutToClose;
             OrderSignal actualOrder = OrderSignal.doNothing;
 
+            int i = 0;
             foreach (string symbol in Symbols)
             {
                 isMarketAboutToClose = !theMarket.DateTimeIsOpen(Time.AddMinutes(10));
@@ -78,28 +106,46 @@ namespace QuantConnect.Algorithm.CSharp.JJAlgorithms.DecycleInverseFisher
                 {
                     if (noOvernight && isMarketAboutToClose)
                     {
-                        if (Strategies[symbol].Position == StockState.longPosition) actualOrder = OrderSignal.closeLong;
-                        else if (Strategies[symbol].Position == StockState.shortPosition) actualOrder = OrderSignal.closeShort;
+                        if (Strategy[symbol].Position == StockState.longPosition) actualOrder = OrderSignal.closeLong;
+                        else if (Strategy[symbol].Position == StockState.shortPosition) actualOrder = OrderSignal.closeShort;
                         else actualOrder = OrderSignal.doNothing;
                     }
                     else
                     {
                         // Now check if there is some signal and execute the strategy.
-                        actualOrder = Strategies[symbol].CheckSignal();
+                        actualOrder = Strategy[symbol].CheckSignal();
                     }
                     ExecuteStrategy(symbol, actualOrder, data);
                 }
+                #region Logging stuff - Filling the data StockLogging
+                //Counter, Time, Close, Decycle, InvFisher, OrderSignal, StateFromStrategy, StateFromPorfolio
+                string newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7}",
+                                               barCounter,
+                                               Time,
+                                               data[symbol].Close,
+                                               Strategy[symbol].DecycleTrend.Current.Value,
+                                               Strategy[symbol].InverseFisher.Current.Value,
+                                               (actualOrder == OrderSignal.goLong || actualOrder == OrderSignal.closeShort) ? 1 :
+                                               (actualOrder == OrderSignal.goShort || actualOrder == OrderSignal.closeLong) ? -1 : 0,
+                                               Strategy[symbol].Position.ToString(),
+                                               Portfolio[symbol].Quantity.ToString(),
+                                               Portfolio.TotalPortfolioValue
+                                               );
+                stockLogging[i].AppendLine(newLine);
+                i++;
+                #endregion Logging stuff - Filling the data StockLogging
             }
+            barCounter++; // just for logging
         }
 
         public override void OnOrderEvent(OrderEvent orderEvent)
         {
             string symbol = orderEvent.Symbol;
             int position = Portfolio[symbol].Quantity;
-            
-            if (position > 0) Strategies[symbol].Position = StockState.longPosition;
-            else if (position < 0) Strategies[symbol].Position = StockState.shortPosition;
-            else Strategies[symbol].Position = StockState.noInvested;
+
+            if (position > 0) Strategy[symbol].Position = StockState.longPosition;
+            else if (position < 0) Strategy[symbol].Position = StockState.shortPosition;
+            else Strategy[symbol].Position = StockState.noInvested;
         }
 
         public override void OnEndOfDay()
@@ -108,22 +154,45 @@ namespace QuantConnect.Algorithm.CSharp.JJAlgorithms.DecycleInverseFisher
             {
                 foreach (string symbol in Symbols)
                 {
-                    Strategies[symbol].Reset();
+                    Strategy[symbol].Reset();
                 }
             }
         }
-        
+
+        public override void OnEndOfAlgorithm()
+        {
+            #region Logging stuff - Saving the logs
+
+            int i = 0;
+            foreach (string symbol in Symbols)
+            {
+                string filename = string.Format("LittleWing{0}.csv", symbol);
+                // JJ do not delete this line it locates my engine\bin\debug folder
+                //  I just uncomment it when I run on my local machine
+                string filePath = AssemblyLocator.ExecutingDirectory() + filename;
+
+                if (File.Exists(filePath)) File.Delete(filePath);
+                File.AppendAllText(filePath, stockLogging[i].ToString());
+                Debug(string.Format("\nSymbol Name: {0}, Ending Value: {1} ", symbol, Portfolio[symbol].Profit));
+
+            }
+
+            Debug(string.Format("\nAlgorithm Name: {0}\n Ending Portfolio Value: {1} ", this.GetType().Name, Portfolio.TotalPortfolioValue));
+
+            #endregion Logging stuff - Saving the logs
+        }
+
         #endregion
 
         #region Algorithm methods
-        
+
         private void RegisterStrategy(string symbol)
         {
             var consolidator = new IdentityDataConsolidator<TradeBar>();
             SubscriptionManager.AddConsolidator(symbol, consolidator);
             consolidator.DataConsolidated += (sender, consolidated) =>
             {
-                Strategies[symbol].DecycleTrend.Update(new IndicatorDataPoint(consolidated.Time, consolidated.Price));
+                Strategy[symbol].DecycleTrend.Update(new IndicatorDataPoint(consolidated.Time, consolidated.Price));
             };
         }
 
@@ -149,7 +218,7 @@ namespace QuantConnect.Algorithm.CSharp.JJAlgorithms.DecycleInverseFisher
                 case OrderSignal.closeShort:
                     Liquidate(symbol);
                     break;
-                
+
                 default: break;
             }
         }
